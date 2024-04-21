8082073f58338f1198ce327be3edb3df5d712c48
JPodgo <j.podgorski@fastmail.com>
2024-04-21 12:52:48 +0200

    Initial test-dump of the entire website

---
+++ wp-admin/edit.php
@@ -0,0 +1,7 @@
+<html>
+    <head>
+        <title>Redirecting</title>
+        <meta http-equiv="refresh" content="0;url=../wp-login.php?redirect_to=&reauth=1" />
+    </head>
+    <body></body>
+</html><!-- Powered by Staatic (https://staatic.com/) -->
